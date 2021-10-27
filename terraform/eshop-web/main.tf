5537fbdb87f367bcc61ae27d2a7a5d2f0a0c4855
Amal Abeygunawardana <amalabey@gmail.com>
2021-10-27 22:13:38 +1100

    Add ECS task and service

---
+++ terraform/eshop-web/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  backend "s3" {
+  }
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.27"
+    }
+  }
+  required_version = ">= 1.0.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-southeast-2"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+      Name = "eshop-ecs-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "eshop-ecs-igw"
+  }
+}
+
+resource "aws_route" "route" {
+  route_table_id = aws_vpc.vpc.main_route_table_id
+  gateway_id = aws_internet_gateway.igw.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.0.0/20"
+}
+
+resource "aws_security_group" "sg" {
+  name = "ecs"
+  vpc_id = aws_vpc.vpc.id
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 0
+    self = true
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [""]
+  }
+  ingress {
+    from_port = 80
+    self = false
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
