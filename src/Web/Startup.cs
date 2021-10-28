663e86d4fd837ae2feabd26904bbdfa33c232f9e
Amal Abeygunawardana <amalabey@gmail.com>
2021-10-28 21:29:50 +1100

    Enable in-memory db for simplicity

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -129,7 +129,8 @@ namespace Microsoft.eShopWeb.Web
                 config.Path = "/allservices";
             });
 
-            ConfigureProductionServices(services);
+            //ConfigureProductionServices(services);
+            ConfigureTestingServices(services);
 
             _services = services; // used to debug registered services
         }
