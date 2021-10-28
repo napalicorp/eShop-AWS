1654ef0e6b5b11879f9aed04a2007b1bfcb6030f
Amal Abeygunawardana <amalabey@gmail.com>
2021-10-28 23:53:03 +1100

    Move ecr back to shared infra module

--- terraform/shared-infra/main.tf
+++ terraform/shared-infra/main.tf
@@ -21,3 +21,12 @@ resource "aws_s3_bucket" "bucket" {
     enabled = true
   }
 }
+
+resource "aws_ecr_repository" "ecr" {
+  name                 = "${var.env_prefix}-eshop-ecr"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
