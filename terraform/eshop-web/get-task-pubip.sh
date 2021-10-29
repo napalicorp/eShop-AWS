dddd80f471f9a4e16881a079b8cc83fa893d28ba
Amal Abeygunawardana <amalabey@gmail.com>
2021-10-29 17:24:28 +1100

    Fix script to retrieve ecs task pub ip

--- terraform/eshop-web/get-task-pubip.sh
+++ terraform/eshop-web/get-task-pubip.sh
@@ -5,5 +5,6 @@ service_name=`terraform -chdir=terraform/eshop-web output -json service_name | j
 task_arn=`aws ecs list-tasks --cluster $cluster_arn --service-name $service_name | jq -r ".taskArns[0]"`
 echo "Task Id: $task_arn"
 task_ni_id=`aws ecs describe-tasks --cluster $cluster_arn --tasks $task_arn --output json | jq -r '.tasks[0].attachments[0].details[] | select(.name == "networkInterfaceId") | .value'`
-task_pubip=`aws ec2 describe-network-interfaces --network-interface-ids $test | jq -r ".NetworkInterfaces[0].Association.PublicIp"`
+echo "Network interface id: $task_ni_id"
+task_pubip=`aws ec2 describe-network-interfaces --network-interface-ids $task_ni_id | jq -r ".NetworkInterfaces[0].Association.PublicIp"`
 echo "http://$task_pubip"
